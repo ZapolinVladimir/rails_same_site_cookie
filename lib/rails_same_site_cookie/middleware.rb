05d4e88645d3f151f3e196272d80f813dd72fc8f
Vladimir <32322174+ZapolinVladimir@users.noreply.github.com>
2020-02-11 19:04:29 +0600

    Update middleware.rb

--- lib/rails_same_site_cookie/middleware.rb
+++ lib/rails_same_site_cookie/middleware.rb
@@ -13,32 +13,33 @@ module RailsSameSiteCookie
       status, headers, body = @app.call(env)
       puts "STATUS", status
       puts "HEADERS", headers
-      puts "BODY", body
+      puts "BODY", body.inspect
       regex = RailsSameSiteCookie.configuration.user_agent_regex
-      
-      if headers['Set-Cookie'].present? 
-#         and (regex.nil? or regex.match(env['HTTP_USER_AGENT']))
-        parser = UserAgentChecker.new(env['HTTP_USER_AGENT'])
-        puts "PARSER", parser
-        if parser.send_same_site_none?
-          cookies = headers['Set-Cookie'].split(COOKIE_SEPARATOR)
-          ssl = Rack::Request.new(env).ssl?
+      unless headers['Location'].include?('admin/oauth/authorize')
+        if headers['Set-Cookie'].present? 
+  #         and (regex.nil? or regex.match(env['HTTP_USER_AGENT']))
+          parser = UserAgentChecker.new(env['HTTP_USER_AGENT'])
+          puts "PARSER", parser
+          if parser.send_same_site_none?
+            cookies = headers['Set-Cookie'].split(COOKIE_SEPARATOR)
+            ssl = Rack::Request.new(env).ssl?
 
-          cookies.each do |cookie|
-            next if cookie.blank?
-#             if ssl and not cookie =~ /;\s*secure/i
-             if not cookie =~ /;\s*secure/i
-              cookie << '; secure'
+            cookies.each do |cookie|
+              next if cookie.blank?
+  #             if ssl and not cookie =~ /;\s*secure/i
+               if not cookie =~ /;\s*secure/i
+                cookie << '; secure'
+              end
+
+              unless cookie =~ /;\s*samesite=/i
+                cookie << '; SameSite=None'
+              end
+
             end
 
-            unless cookie =~ /;\s*samesite=/i
-              cookie << '; SameSite=None'
-            end
-
+            headers['Set-Cookie'] = cookies.join(COOKIE_SEPARATOR)
+            puts "HEADERS RESULT", headers
           end
-
-          headers['Set-Cookie'] = cookies.join(COOKIE_SEPARATOR)
-          puts "HEADERS RESULT", headers
         end
       end
 
